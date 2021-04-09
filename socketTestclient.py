85ece74beb6046df1d9ba7431b9964ed2e78e1df
DevCode-C <emmanuelparedescamargo@gmail.com>
2021-04-09 18:14:48 -0500

    New information added

--- socketTestclient.py
+++ socketTestclient.py
@@ -1,13 +1,27 @@
 import socket
 
+HEARDSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(),1234))
 
-full_msg = ''
-
 while True:
-    msg = s.recv(8)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode("utf-8")
-print(full_msg) 
+
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"New Message length: {msg[:HEARDSIZE]}")
+            msglen = int(msg[:HEARDSIZE])
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEARDSIZE == msglen:
+            print("Full msg recvd")
+            print(full_msg[HEARDSIZE:])
+            new_msg = True
+            full_msg = ''
+
+    print(full_msg) 
